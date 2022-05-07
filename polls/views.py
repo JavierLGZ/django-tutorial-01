0662b20392b941542a2d85eb0ac0ffc01dcd7877
JavierLGZ <javierl_gonzalezz@outlook.es>
2022-05-06 20:39:52 -0500

    creando vistas generando plantillas y reescribiendo funciones mas pythonistas

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 
-# Create your views here.
+
 def index(request):
-    return HttpResponse("hello world you're at the polls index.")
+    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
+    return render(request, "polls/index.html", {
+        "lastest_question_list": lastest_question_list
+    })
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
+
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at results of questions {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting at question {question_id}")
